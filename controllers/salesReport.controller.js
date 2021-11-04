d32e25befd1680e8f614a25f869c774be5903a9d
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2021-11-04 22:47:42 +0500

    completed sales report

--- controllers/salesReport.controller.js
+++ controllers/salesReport.controller.js
@@ -9,48 +9,132 @@ const AppError = require('../utils/AppError');
 
 module.exports.getAll = catchAsync(async function (req, res, next) {
     const { startDate, endDate } = req.query;
-    let [rawMaterialExpenses, shopExpenses, salariesExpenses, bills] = await Promise.all([
-        await mongoose.model('RawMaterialExpenses').aggregate([
-            {
-                $match: { createdAt: { $gte: startDate, $lte: endDate } },
-            },
-            { $group: { _id: null, price: { $sum: '$price' } } },
-        ]),
-        await mongoose.model('ShopExpenses').aggregate([
-            {
-                $match: { createdAt: { $gte: startDate, $lte: endDate } },
-            },
-            { $group: { _id: null, price: { $sum: '$price' } } },
-        ]),
-        await mongoose.model('SalariesExpense').aggregate([
-            {
-                $match: { createdAt: { $gte: startDate, $lte: endDate } },
-            },
-            { $group: { _id: null, price: { $sum: '$amount' } } },
-        ]),
-        await mongoose.model('Bill').find({ createdAt: { $gte: startDate, $lte: endDate } }),
-    ]);
-    // let sales =[ {
-    //     product,
-    //     group,
-    //     qty,
-    //     sellPrice,
-    //     costPrice
-    // }]
 
-    if (bills) {
-        bills.forEach((bill) => {
-            bill.products.forEach((product) => {
-                console.log(product.product.name);
-            });
-        });
-        return res.send(bills);
+    const getExpenses = async () => {
+        let [rawMaterialExpenses, shopExpenses, salariesExpenses, bills, refundBills] = await Promise.all([
+            await mongoose.model('RawMaterialExpenses').aggregate([
+                {
+                    $match: { createdAt: { $gte: startDate, $lte: endDate } },
+                },
+                { $group: { _id: null, price: { $sum: '$price' } } },
+            ]),
+            await mongoose.model('ShopExpenses').aggregate([
+                {
+                    $match: { createdAt: { $gte: startDate, $lte: endDate } },
+                },
+                { $group: { _id: null, price: { $sum: '$price' } } },
+            ]),
+            await mongoose.model('SalariesExpense').aggregate([
+                {
+                    $match: { createdAt: { $gte: startDate, $lte: endDate } },
+                },
+                { $group: { _id: null, price: { $sum: '$amount' } } },
+            ]),
+        ]);
+
+        rawMaterialExpenses = rawMaterialExpenses[0].price;
+        shopExpenses = shopExpenses[0].price;
+        salariesExpenses = salariesExpenses[0].price;
+        const totalExpenses = rawMaterialExpenses + shopExpenses + salariesExpenses;
+        return { rawMaterialExpenses, shopExpenses, salariesExpenses, totalExpenses };
     }
 
-    rawMaterialExpenses = rawMaterialExpenses[0].price;
-    shopExpenses = shopExpenses[0].price;
-    salariesExpenses = salariesExpenses[0].price;
-    const totalExpenses = rawMaterialExpenses + shopExpenses + salariesExpenses;
+    const getProducts = async () => {
 
-    res.status(200).send({ expenses: { rawMaterialExpenses, shopExpenses, salariesExpenses, totalExpenses } });
+        const getOriginalProducts = async () => {
+            const bills = await mongoose.model('Bill').find({ createdAt: { $gte: startDate, $lte: endDate } }).lean();
+            let dbOriginalProducts = [];
+
+            bills.forEach((bill) => {
+                dbOriginalProducts.push(
+                    bill.products.map((p) => {
+                        const amount = p.amount - (p.amount * (bill.discountPercent / 100));
+                        return { ...p.product, qty: p.qty, amount };
+                    })
+                );
+            });
+
+            dbOriginalProducts = _.flattenDeep(dbOriginalProducts);
+
+            const groupedOriginalProducts = [];
+
+            for (const originalProduct of dbOriginalProducts) {
+
+                const productIndex = groupedOriginalProducts.findIndex((p) => p._id.toString() === originalProduct._id.toString());
+
+                if (productIndex !== -1) {
+                    const existingProduct = groupedOriginalProducts[productIndex];
+                    existingProduct.qty += originalProduct.qty;
+                    existingProduct.amount += originalProduct.amount;
+                } else {
+                    groupedOriginalProducts.push(originalProduct);
+                }
+
+            }
+
+            return groupedOriginalProducts;
+        }
+
+        const getRefundProducts = async () => {
+            const refundBills = await mongoose.model('RefundBill').find({ createdAt: { $gte: startDate, $lte: endDate } }).lean();
+            let dbRefundProducts = [];
+
+            refundBills.forEach((refundBill) => {
+                dbRefundProducts.push(
+                    refundBill.products.map((p) => {
+                        // const amount = p.amount - (p.amount * (refundBill.discountPercent / 100));
+                        return { ...p, amount: p.amount };
+                    })
+                );
+            });
+
+            dbRefundProducts = _.flattenDeep(dbRefundProducts);
+
+            const groupedRefundProducts = [];
+
+            for (const refundProduct of dbRefundProducts) {
+
+                const productIndex = groupedRefundProducts.findIndex((p) => p._id.toString() === refundProduct._id.toString());
+
+                if (productIndex !== -1) {
+                    const existingProduct = groupedRefundProducts[productIndex];
+                    existingProduct.qty += refundProduct.qty;
+                    existingProduct.amount += refundProduct.amount;
+                } else {
+                    groupedRefundProducts.push(refundProduct);
+                }
+
+            }
+
+            return groupedRefundProducts;
+        }
+
+        const deductRefunds = (o, refunds) => {
+            const originals = [...o];
+
+            refunds.forEach((refundProduct) => {
+
+                const originalProductIndex = originals.findIndex((o) => o._id.toString() === refundProduct._id.toString());
+
+
+                if (originalProductIndex > -1) {
+                    console.log(originals[originalProductIndex]);
+                    originals[originalProductIndex].qty -= refundProduct.qty;
+                    originals[originalProductIndex].amount -= refundProduct.amount;
+                }
+            });
+
+            return originals;
+        }
+
+        const [originalProducts, refundProducts] = await Promise.all([getOriginalProducts(), getRefundProducts()]);
+
+        const deductedProducts = deductRefunds(originalProducts, refundProducts);
+
+        return deductedProducts;
+    }
+
+    const [expenses, products] = await Promise.all([getExpenses(), getProducts()]);
+
+    res.status(200).send({ expenses, products });
 });
